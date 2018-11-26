a105ae64b5cbd4116732769fcf8da25c91083b23
Withee Poositasai <witheep@gmail.com>
2018-11-26 16:52:20 +0700

    Add basic gate operation UI

---
+++ src/components/GateIcon.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="gate-icon" @click="onClickGate()">
+    <h1>{{ symbol.toUpperCase() }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GateIcon',
+  props: ['symbol', 'onClickGate']
+}
+</script>
+
+<style lang="scss" scoped>
+  .gate-icon {
+    width: 80px;
+    height: 80px;
+    display: flex;
+    justify-content: center;
+    background-color: white;
+    color: black;
+    margin: 10px;
+
+    h1 {
+      margin: auto;
+    }
+  }
+</style>
--- src/views/Composer.vue
+++ src/views/Composer.vue
@@ -1,21 +1,71 @@
 <template>
   <div class="fx-col composer">
+    <!-- title row -->
     <div class="fx-row">
       <h3 class="title">Quantarium | Composer</h3>
-    </div>
-    <div class="fx-row fx-x2">stage</div>
+    </div><!-- end of title row -->
+    <!-- stage -->
+    <div class="fx-row fx-x2">
+      <GateIcon v-for="(gateSym, g) in appliedGates" :key="g" :symbol="gateSym" />
+      <span v-if="appliedGates.length > 0" @click="removeGate()">Remove</span>
+    </div><!-- end of stage -->
+    <!-- controller row -->
     <div class="fx-row fx-x1">
-      <div class="fx-col fx-x2">gate panel</div>
-      <div class="fx-col fx-x1">option</div>
-    </div>
+      <!-- gate tray -->
+      <div class="fx-col fx-x2">
+        <div class="fx-row">
+          <!-- gates list -->
+          <div class="fx-col fx-x2">
+            <div class="fx-row">
+              <GateIcon v-for="gate in availableGates" :key="gate.symbol" :symbol="gate.symbol" :onClickGate="() => onClickGate(gate)" />
+            </div>
+          </div><!-- end of gates list -->
+          <div class="fx-col fx-x1">
+            {{ focusedGate }}
+          </div>
+        </div>
+      </div><!-- end of gate tray -->
+      <!-- options -->
+      <div class="fx-col fx-x1">
+        option
+      </div><!-- end of options -->
+    </div><!-- end of controller row -->
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
+import GateIcon from '../components/GateIcon'
 
 export default {
-  name: 'Composer'
+  name: 'Composer',
+  components: {
+    GateIcon
+  },
+  data () {
+    return {
+      availableGates: [
+        { symbol: 'h', name: 'Hadamard Gate', desc: 'desc' },
+        { symbol: 'x', name: 'Pauli-X Gate', desc: 'desc' },
+        { symbol: 'y', name: 'Pauli-Y Gate', desc: 'desc' },
+        { symbol: 'z', name: 'Pauli-Z Gate', desc: 'desc' }
+      ],
+      appliedGates: [],
+      focusedGate: {}
+    }
+  },
+  methods: {
+    onClickGate (gate) {
+      if (gate !== this.focusedGate) {
+        this.focusedGate = gate
+      } else {
+        this.appliedGates.push(gate.symbol)
+        this.focusedGate = {}
+      }
+    },
+    removeGate () {
+      this.appliedGates.splice(this.appliedGates.length - 1, 1)
+    }
+  }
 }
 </script>
 
@@ -29,6 +79,7 @@ export default {
 
     .title {
       text-align: center;
+      margin: 10px auto;
     }
   }
 </style>
